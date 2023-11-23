6ce5abb128384731180e3fc9160c19e9f61aae4a
SxntX1623 <127440542+SxntX1623@users.noreply.github.com>
2023-11-23 11:29:26 -0500

    Add files via upload

--- PAGINA-JSON/index.js
+++ PAGINA-JSON/index.js
@@ -4,7 +4,7 @@ import { data } from './DATA/data.js';
 const items = document.getElementById('items');
 const templateCard = document.getElementById('template-card').content;
 const fragment = document.createDocumentFragment();
-let like = {};
+let like = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     loadData(data);
@@ -21,4 +21,29 @@ const loadData = data => {
         fragment.appendChild(clone);
     })
     items.appendChild(fragment);
+}
+
+items.addEventListener('click', e => {
+    addLike(e);
+})
+
+const addLike = e => {
+    
+    if (e.target.classList.contains('btn-dark')){
+        setLike(e.target.parentElement);
+    }
+}
+
+const setLike = objeto => {
+    const boton = {
+        id: objeto.querySelector('.btn-dark').dataset.id,
+        cantidad: 0
+    }
+
+    if (like.hasOwnProperty(boton.id)){
+        boton.cantidad = like[boton.id].cantidad + 1;
+        objeto.querySelector('#like').textContent = boton.cantidad;
+    }
+    like[boton.id] = {...boton};
+    console.log(like[boton.id]);
 }
